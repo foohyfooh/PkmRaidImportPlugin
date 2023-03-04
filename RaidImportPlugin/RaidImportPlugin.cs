ffe41734a6ff53f9d3a24cba2c7cb7011c0aed52
Jonathan Herbert <foohyfooh@gmail.com>
2023-03-03 22:05:03 -0400

    Deduplicate Logic And Flag Save Edited

--- RaidImportPlugin/RaidImportPlugin.cs
+++ RaidImportPlugin/RaidImportPlugin.cs
@@ -7,6 +7,9 @@ namespace RaidImportPlugin {
     public ISaveFileProvider SaveFileEditor { get; private set; } = null!;
     public IPKMView PKMEditor { get; private set; } = null!;
     private ToolStripMenuItem ImportRaidButton = null!;
+    private bool IsCompatibleSave { 
+      get { return SaveFileEditor.SAV is SAV8SWSH or SAV9SV; }
+    }
 
     public void Initialize(params object[] args) {
       Console.WriteLine($"Loading {Name}...");
@@ -15,7 +18,7 @@ namespace RaidImportPlugin {
       ToolStrip menu = (ToolStrip)Array.Find(args, z => z is ToolStrip)!;
       ToolStripDropDownItem tools = (ToolStripDropDownItem)menu.Items.Find("Menu_Tools", false)[0]!;
       ImportRaidButton = new ToolStripMenuItem("Import Raid");
-      ImportRaidButton.Available = SaveFileEditor.SAV is SAV8SWSH || SaveFileEditor.SAV is SAV9SV;
+      ImportRaidButton.Available = IsCompatibleSave;
       ImportRaidButton.Click += (s, e) => ImportRaid();
       tools.DropDownItems.Add(ImportRaidButton);
     }
@@ -23,66 +26,45 @@ namespace RaidImportPlugin {
     private void ImportRaid() {
       FolderBrowserDialog dialog = new FolderBrowserDialog();
       DialogResult dialogResult = dialog.ShowDialog();
-      if(dialogResult == DialogResult.OK) {
-        if (SaveFileEditor.SAV is SAV8SWSH) ImportSwShRaid(dialog.SelectedPath);
-        else if (SaveFileEditor.SAV is SAV9SV) ImportSVRaid(dialog.SelectedPath);
+      if (dialogResult == DialogResult.OK) {
+        string raidPath = dialog.SelectedPath;
+        string RaidFilePath(string file) => $@"{raidPath}\{file}";
+        List<Tuple<uint, string>> blocks = null!;
+        if (SaveFileEditor.SAV is SAV8SWSH sav8swsh) {
+          blocks = new List<Tuple<uint, string>> () { 
+            new Tuple<uint, string>(SwShConstants.BonusRewardsLocation, RaidFilePath(SwShConstants.BonusRewardsStr)),
+            new Tuple<uint, string>(SwShConstants.DiaEncounterLocation, RaidFilePath(SwShConstants.DiaEncounterStr)),
+            new Tuple<uint, string>(SwShConstants.DropRewardsLocation, RaidFilePath(SwShConstants.DropRewardsStr)),
+            new Tuple<uint, string>(SwShConstants.NormalEncountLocation, RaidFilePath(SwShConstants.NormalEncountStr)),
+          };
+          if (sav8swsh.SaveRevision >= 1) blocks.Add(new Tuple<uint, string>(SwShConstants.NormalEncountRigel1Location, RaidFilePath(SwShConstants.NormalEncountRigel1Str)));
+          if (sav8swsh.SaveRevision >= 2) blocks.Add(new Tuple<uint, string>(SwShConstants.NormalEncountRigel2Location, RaidFilePath(SwShConstants.NormalEncountRigel2Str)));
+        } else if (SaveFileEditor.SAV is SAV9SV) {
+          raidPath += @"\Files";
+          blocks = new List<Tuple<uint, string>> () {
+            new Tuple<uint, string>(SVConstants.EventRaidIdentifierLocation, RaidFilePath(SVConstants.EventRaidIdentifierStr)),
+            new Tuple<uint, string>(SVConstants.FixedRewardItemArrayLocation, RaidFilePath(SVConstants.FixedRewardItemArrayStr)),
+            new Tuple<uint, string>(SVConstants.LotteryRewardItemArrayLocation, RaidFilePath(SVConstants.LotteryRewardItemArrayStr)),
+            new Tuple<uint, string>(SVConstants.RaidEnemyArrayLocation, RaidFilePath(SVConstants.RaidEnemyArrayStr)),
+            new Tuple<uint, string>(SVConstants.RaidPriorityArrayLocation, RaidFilePath(SVConstants.RaidPriorityArrayStr))
+          };
+        }
+        ImportRaid(raidPath, (dynamic)SaveFileEditor.SAV, blocks);
       }
     }
 
-    private bool HasSwShRaidFiles(string raidPath) {
-      bool FileExists(string blockPath) => File.Exists($@"{raidPath}\{blockPath}");
-      if (!FileExists(SwShConstants.BonusRewardsStr)) return false;
-      if (!FileExists(SwShConstants.DiaEncounterStr)) return false;
-      if (!FileExists(SwShConstants.DropRewardsStr)) return false;
-      if (!FileExists(SwShConstants.NormalEncountStr)) return false;
-      SAV8SWSH sav = (SAV8SWSH)SaveFileEditor.SAV;
-      if (sav.SaveRevision >= 1 && !FileExists(SwShConstants.NormalEncountRigel1Str)) return false;
-      if (sav.SaveRevision >= 2 && !FileExists(SwShConstants.NormalEncountRigel2Str)) return false;
-      return true;
-    }
-
-    private void ImportSwShRaid(string raidPath) {
-      SAV8SWSH sav = (SAV8SWSH)SaveFileEditor.SAV;
-      void ImportBlock(uint location, string blockPath) => sav.Accessor.GetBlock(location).ChangeData(File.ReadAllBytes($@"{raidPath}\{blockPath}"));
-      if (HasSwShRaidFiles(raidPath)) {
-        ImportBlock(SwShConstants.BonusRewardsLocation, SwShConstants.BonusRewardsStr);
-        ImportBlock(SwShConstants.DiaEncounterLocation, SwShConstants.DiaEncounterStr);
-        ImportBlock(SwShConstants.DropRewardsLocation, SwShConstants.DropRewardsStr);
-        ImportBlock(SwShConstants.NormalEncountLocation, SwShConstants.NormalEncountStr);
-        if (sav.SaveRevision >= 1) ImportBlock(SwShConstants.NormalEncountRigel1Location, SwShConstants.NormalEncountRigel1Str);
-        if (sav.SaveRevision >= 2) ImportBlock(SwShConstants.NormalEncountRigel2Location, SwShConstants.NormalEncountRigel2Str);
-        MessageBox.Show("Raid Imported");
+    private static void ImportRaid<S>(string raidPath, S sav, List<Tuple<uint, string>> blocks) where S: SaveFile, ISCBlockArray, ISaveFileRevision {
+      if (blocks.All(b => File.Exists(b.Item2))) {
+        foreach ((uint blockLocation, string file) in blocks)
+          sav.Accessor.GetBlock(blockLocation).ChangeData(File.ReadAllBytes(file));
+        sav.State.Edited = true;
+        MessageBox.Show("Raid Imported", "Raid Importer");
       } else {
-        MessageBox.Show($"Ensure that all necessary files are in {raidPath}", "Raid Importer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        MessageBox.Show($@"Ensure that all necessary files are in {raidPath}", "Raid Importer", MessageBoxButtons.OK, MessageBoxIcon.Error);
       }
     }
 
-    private bool HasSVRaidFiles(string raidPath) {
-      bool FileExists(string blockPath) => File.Exists($@"{raidPath}\Files\{blockPath}");
-      if (!FileExists(SVConstants.EventRaidIdentifierStr)) return false;
-      if (!FileExists(SVConstants.FixedRewardItemArrayStr)) return false;
-      if (!FileExists(SVConstants.LotteryRewardItemArrayStr)) return false;
-      if (!FileExists(SVConstants.RaidEnemyArrayStr)) return false;
-      if (!FileExists(SVConstants.RaidPriorityArrayStr)) return false;
-      return true;
-    }
-
-    private void ImportSVRaid(string raidPath)  {
-      SAV9SV sav = (SAV9SV)SaveFileEditor.SAV;
-      void ImportBlock(uint location, string blockPath) => sav.Accessor.GetBlock(location).ChangeData(File.ReadAllBytes($@"{raidPath}\Files\{blockPath}"));
-      if (HasSVRaidFiles(raidPath)) {
-        ImportBlock(SVConstants.EventRaidIdentifierLocation, SVConstants.EventRaidIdentifierStr);
-        ImportBlock(SVConstants.FixedRewardItemArrayLocation, SVConstants.FixedRewardItemArrayStr);
-        ImportBlock(SVConstants.LotteryRewardItemArrayLocation, SVConstants.LotteryRewardItemArrayStr);
-        ImportBlock(SVConstants.RaidEnemyArrayLocation, SVConstants.RaidEnemyArrayStr);
-        ImportBlock(SVConstants.RaidPriorityArrayLocation, SVConstants.RaidPriorityArrayStr);
-        MessageBox.Show("Raid Imported");
-      } else {
-        MessageBox.Show($@"Ensure that all necessary files are in {raidPath}\Files", "Raid Importer", MessageBoxButtons.OK, MessageBoxIcon.Error);
-      }
-    }
-
-    public void NotifySaveLoaded() => ImportRaidButton.Available = SaveFileEditor.SAV is SAV8SWSH || SaveFileEditor.SAV is SAV9SV;
+    public void NotifySaveLoaded() => ImportRaidButton.Available = IsCompatibleSave;
     public bool TryLoadFile(string filePath) => false;
   }
 }
